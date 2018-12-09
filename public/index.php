2a1dcbfe5600a75acda160c011788edc56431ca7
Justin Ho <justinhodev@gmail.com>
2018-12-08 16:57:00 -0800

    Update index.php

--- public/index.php
+++ public/index.php
@@ -16,16 +16,16 @@
 
 <div class="container">
   <div class="row my-3 justify-content-end">
-    <div class="dropdown">
-      <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        sort by
-      </button>
-      <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-        <a class="dropdown-item" href="#">Newest First</a>
-        <a class="dropdown-item" href="#">Most Popular</a>
-        <a class="dropdown-item" href="#">Price</a>
+    <form>
+      <div class="form-group">
+        <label for="sort-by">Sort By</label>
+        <select id="sort-by" class="form-control" onchange="sortBy(this.value)">
+          <option value="newest">Newest</option>
+          <option value="most-popular">Most Popular</option>
+          <option value="name">Name</option>
+        </select>
       </div>
-    </div>
+    </form>
   </div>
   <div class="row mb-5">
       <?php while($shoe = mysqli_fetch_assoc($shoe_set)) { ?>
@@ -33,7 +33,7 @@
           <div class="card text-center px-4" style="width: 15rem;">
           <?php echo '<img src="data:image/jpeg;base64,'.base64_encode( $shoe['image'] ).'" class="card-img-top"/>'; ?>
             <div class="card-body">
-              <h5 class="card-title"><?php echo h($shoe['sneaker_name']); ?></h5>
+              <h6 class="card-title"><?php echo h($shoe['sneaker_name']); ?></h6>
               <h6 class="card-subtitle text-muted"><?php echo h($shoe['brand_name']); ?></p>
               <a href="./details.php?id=<?php echo urlencode($shoe['sneaker_id']); ?>&name=<?php echo urlencode($shoe['sneaker_name']); ?>" class="card-link">Details</a>
             </div>
