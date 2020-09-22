647808b3ccfbbc3ae3555b2210c4b3ac3e89db22
Justin Ho <justinhodev@gmail.com>
2020-09-21 22:44:45 -0700

    feat(db): add member model

---
+++ app/Models/Member.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Member extends Model
+{
+    use HasFactory;
+    /**
+     * Use PascalCase for tables
+     *
+     * @var string
+     */
+    protected $table = 'Members';
+
+    /**
+     * Primary Key - use name
+     *
+     * @var string
+     */
+    protected $primaryKey = 'Email';
+
+    /**
+     * Change PK type to string
+     *
+     * @var string
+     */
+    protected $keyType = 'string';
+
+    /**
+     * Stop auto-increment for non-int PK
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+}
