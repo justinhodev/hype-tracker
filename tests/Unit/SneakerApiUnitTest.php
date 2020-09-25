862d28f11f09e3679447c20f491f12df573a7387
Justin Ho <justinhodev@gmail.com>
2020-09-24 23:37:06 -0700

    test(http): add test case for sneaker api

---
+++ tests/Unit/SneakerApiUnitTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+
+/**
+ * @group unit
+ */
+class SneakerApiUnitTest extends TestCase
+{
+    /**
+     * Get Sneaker By Id
+     *
+     * @return void
+     */
+    public function testGetSneakerById()
+    {
+        $response = $this->json('GET', '/api/sneakers');
+        $response->assertStatus(200);
+    }
+}
