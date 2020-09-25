46ecad3db59f732aa02f1c2a540f3d1f4a654804
Justin Ho <justinhodev@gmail.com>
2020-09-24 21:48:09 -0700

    refactor(db): migrate old hypetracker db to laravel (#4)
    * feat(db): add sneaker model
    
    - orm for Sneakers table
    
    * feat(db): add sneaker table migration
    
    * chore(db): remove sample db tables
    
    - remove user and password resets table and related
    
    * feat(db): add members table migration
    
    * feat(db): add watchlists table migration
    
    * feat(db): add rankings table migration
    
    * feat(db): add member model
    
    * feat(db): add rankings model
    
    - see comments on model file about composite primary key implementation
    
    * feat(db): add accessors for all models
    
    * feat(db): add mutators for all models
    
    * feat(db): add relationships to models
    
    * feat(db): add casting
    
    - remove redundant accessors and mutators
    
    * feat(db): add factories for all models
    
    * feat(db): add seeders for all models
    
    - fix missing curly brace on member model
    
    * fix(db): refactor migrations
    
    - use auto-incrementing ids to not fight with laravel conventions
    
    * feat(db): finalize all factories and seeders

---
+++ app/Models/Member.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Member extends Model
+{
+    use HasFactory;
+    /**
+     * Use PascalCase for tables
+     *
+     * @var string
+     */
+    protected $table = 'Members';
+
+    /**
+     * Get Member name
+     *
+     * @param string $value
+     * @return string
+     */
+    public function getNameAttribute($value): string
+    {
+        return ucwords($value);
+    }
+
+    /**
+     * Set Member name
+     *
+     * @param string $value
+     * @return void
+     */
+    public function setNameAttribute($value): void
+    {
+        $this->attributes['Name'] = strtolower($value);
+    }
+
+    /**
+     * Get all sneakers for a user
+     *
+     * @return BelongsToMany
+     */
+    public function sneakers(): BelongsToMany
+    {
+        return $this->belongsToMany('App\Models\Sneaker', 'Watchlists', 'MemberId', 'SneakerId');
+    }
+}
---
+++ app/Models/Sneaker.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Sneaker extends Model
+{
+    use HasFactory;
+
+    /**
+     * Use PascalCase for tables
+     *
+     * @var string
+     */
+    protected $table = 'Sneakers';
+
+    /**
+     * The attributes that should be cast
+     *
+     * @var array
+     */
+    protected $casts = [
+        'Price' => 'decimal:2',
+    ];
+
+    /**
+     * Get Sneaker name
+     *
+     * @param string $value
+     * @return string
+     */
+    public function getNameAttribute($value): string
+    {
+        return ucwords($value);
+    }
+
+    /**
+     * Get Sneaker brand
+     *
+     * @param string $value
+     * @return string
+     */
+    public function getBrandAttribute($value): string
+    {
+        return ucwords($value);
+
+    }
+
+    /**
+     * Set Sneaker name
+     *
+     * @param string $value
+     * @return void
+     */
+    public function setNameAttribute($value): void
+    {
+        $this->attributes['Name'] = strtolower($value);
+    }
+
+    /**
+     * Set Sneaker brand
+     *
+     * @param string $value
+     * @return void
+     */
+    public function setBrandAttribute($value): void
+    {
+        $this->attributes['Brand'] = strtolower($value);
+    }
+
+    /**
+     * Get all Rankings for a Sneaker
+     *
+     * @return HasMany
+     */
+    public function rankings(): HasMany
+    {
+        return $this->hasMany('App\Models\Ranking', 'SneakerId', 'id');
+    }
+}
