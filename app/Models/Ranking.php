48afa970d5dc93f82c6874e7057aee7bb845fedd
Justin Ho <justinhodev@gmail.com>
2020-09-26 17:38:02 -0700

    feat(db): add missing relationship on ranking model
    - add inverse one-to-many relation

--- app/Models/Ranking.php
+++ app/Models/Ranking.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Ranking extends Model
 {
@@ -25,4 +26,14 @@ class Ranking extends Model
     protected $dates = [
         'Date',
     ];
+
+    /**
+     * Get Sneaker that owns the Ranking.
+     *
+     * @return BelongsTo
+     */
+    public function sneaker(): BelongsTo
+    {
+        return $this->belongsTo('App\Models\Sneaker');
+    }
 }
