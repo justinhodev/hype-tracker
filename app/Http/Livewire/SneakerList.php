d7d4a4ac70230d04dc411241c6f77d39b0183be3
Justin Ho <justinhodev@gmail.com>
2020-09-30 22:08:53 -0700

    feat(livewire): add data field to sneaker list component

--- app/Http/Livewire/SneakerList.php
+++ app/Http/Livewire/SneakerList.php
@@ -2,12 +2,47 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Sneaker;
+use Illuminate\View\View;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class SneakerList extends Component
 {
-    public function render()
+    use WithPagination;
+
+    const PAGINATE_LENGTH = 12;
+
+    public $search = '';
+
+    protected $updatesQueryString = ['search'];
+
+    public function mount(): void
     {
-        return view('livewire.sneaker-list');
+        $this->fill(request()->only('search'));
+    }
+
+    public function updatingSearch()
+    {
+        $this->resetPage();
+    }
+
+    public function render(): View
+    {
+        return $this->renderSneakerList();
+    }
+
+    public function renderSneakerList(): View
+    {
+        $sneakers = [];
+
+        if (strlen(trim($this->search)) > 0) {
+            $sneakers = Sneaker::where('name', 'LIKE', '%' . $this->search . '%')
+                ->paginate(self::PAGINATE_LENGTH);
+        } else {
+            $sneakers = Sneaker::paginate(self::PAGINATE_LENGTH);
+        }
+
+        return view('livewire.sneaker-list', ['sneakers' => $sneakers]);
     }
 }
