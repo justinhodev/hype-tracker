3642dc27091fddcd6b7bbd60df18d3efa8c17dac
Justin Ho <justinhodev@gmail.com>
2020-10-08 22:23:01 -0700

    feat(livewire): add sneaker details component

---
+++ app/Http/Livewire/SneakerDetail.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Sneaker;
+use Livewire\Component;
+
+class SneakerDetail extends Component
+{
+
+    public $sneaker;
+
+    public function render()
+    {
+        return view('livewire.sneaker-detail')
+            ->extends('layouts.app');
+    }
+
+    public function mount(Sneaker $sneaker)
+    {
+        $this->sneaker = $sneaker;
+    }
+}
--- resources/views/livewire/sneaker-list.blade.php
+++ resources/views/livewire/sneaker-list.blade.php
@@ -21,9 +21,11 @@
                 @foreach ($sneakers as $sneaker)
 
                     <div class="w-full py-2 md:w-1/2 lg:w-1/3 md:px-2">
+                        <a href="/sneaker/{{ $sneaker->id }}">
                         <h3 class="mb-4 font-medium">
                             {{ $sneaker->name }}
                         </h3>
+                        </a>
                     </div>
 
                 @endforeach
