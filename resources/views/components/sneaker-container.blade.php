119c7154103a94460a0616dc6557bb363c55ccdd
Justin Ho <justinhodev@gmail.com>
2020-10-09 17:03:54 -0700

    refactor(blade): separate list item from list view
    - for sneaker list view

---
+++ resources/views/components/sneaker-container.blade.php
@@ -0,0 +1,7 @@
+<div class="w-full py-2 md:w-1/2 lg:w-1/3 md:px-2">
+    <a href="/sneaker/{{ $sneaker->id }}">
+        <h3 class="mb-4 font-medium">
+            {{ $sneaker->name }}
+        </h3>
+    </a>
+</div>
--- resources/views/livewire/sneaker-list.blade.php
+++ resources/views/livewire/sneaker-list.blade.php
@@ -2,29 +2,18 @@
     <div class="flex justify-end w-full">
         <label>
             Search
-            <input
-                wire:model="search"
-                type="text"
-                class="w-1/6 px-3 py-2 leading-tight border rounded shadow text-accent-700 appearance-non focus:outline-none focus:shadow-outline"
-                placeholder="Search Sneakers..."
-            />
+            <input wire:model="search" type="text" class="px-3 py-2 mx-2 leading-tight border rounded shadow text-accent-700 appearance-non focus:outline-none focus:shadow-outline" placeholder="Search Sneakers..." />
         </label>
     </div>
 
     <div class="flex justify-center w-full">
         <div class="flex flex-col p-5 md:flex-wrap md:flex-row">
             @forelse ($sneakers as $sneaker)
-                    <div class="w-full py-2 md:w-1/2 lg:w-1/3 md:px-2">
-                        <a href="/sneaker/{{ $sneaker->id }}">
-                        <h3 class="mb-4 font-medium">
-                            {{ $sneaker->name }}
-                        </h3>
-                        </a>
-                    </div>
+            <x-sneaker-container :sneaker="$sneaker" />
             @empty
-                <div class="w-full">
-                    <h3>Sneaker Not Found</h3>
-                </div>
+            <div class="w-full">
+                <h3>Sneaker Not Found</h3>
+            </div>
             @endforelse
         </div>
     </div>
